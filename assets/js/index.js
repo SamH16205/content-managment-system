5bf0b11998af7f24bbf549c015c4f3f3bbc75445
Sam <samuelhalloum@gmail.com>
2023-05-12 16:55:09 -0500

    requests to db work

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,12 @@
-const inquirer = require('inquirer')
-
-function generateId(){
-    return Math.floor(Math.random(1000))
-}
-
-
-function addDepartment(db){
-    inquirer
-  .prompt([
-    {
+const add_department = [
+  {
       type: 'input',
       name: 'department_name',
-      message: 'Department Name: ',
-    },
-]).then((result) => {
-    db.query(`INSERT INTO departments (department_name, id)
-    VALUES(${result.department_name}, ${generateId()})`)
-})
-}
-
-
-function addRole(db){
-    inquirer
-    .prompt([
+      message: 'Department name: '
+  }
+  ]
+  
+  const add_role = [
       {
         type: 'input',
         name: 'job_title',
@@ -38,99 +22,82 @@ function addRole(db){
         name: 'salary',
         message: 'Yearly Salary: ',
       },
-  ]).then((result) => {
-      db.query(`INSERT INTO roles (job_title, id, department, salary)
-    VALUES(${result.job_title}, ${generateId()}, ${result.department}, ${result.salary})`)
-  })
+  ]
+  
+      const add_employee = [
+          {
+            type: 'input',
+            name: 'first_name',
+            message: 'First Name: ',
+          },
+          {
+            type: 'input',
+            name: 'last_name',
+            message: 'Last Name: ',
+          },
+          {
+            type: 'input',
+            name: 'role',
+            message: 'Role: ',
+          },
+          {
+            type: 'input',
+            name: 'managers',
+            message: 'Managers: ',
+          },
+      ]
+
+function generateId(){
+    return Math.floor(Math.random()*1000000)
 }
 
-function addEmployee(db){
-    inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'first_name',
-        message: 'First Name: ',
-      },
-      {
-        type: 'input',
-        name: 'last_name',
-        message: 'Last Name: ',
-      },
-      {
-        type: 'input',
-        name: 'role',
-        message: 'Role: ',
-      },
-      {
-        type: 'input',
-        name: 'managers',
-        message: 'Managers: ',
-      },
-  ]).then((result) => {
-    db.query(`INSERT INTO employees (id, first_name, last_name, job_title, managers)
-    VALUES(${generateId()}, ${result.first_name}, ${result.last_name}, ${result.job_title}, ${result.managers})`)
-  })
+function addDepartment(db, employee){
+    db.query(`INSERT INTO departments (department_name, id)
+    VALUES("${employee.department_name}", ${generateId()})`)
 }
 
 
-function showDepartments(){
+function addRole (db, employee){
+    db.query(`INSERT INTO roles (job_title, id, department, salary)
+    VALUES("${employee.job_title}", ${generateId()}, "${employee.department}", "${employee.salary}")`)
+}
+
+function addEmployee(db, employee){
+  db.query(`INSERT INTO employees (id, first_name, last_name, job_title, department, salary, managers)
+  VALUES(${generateId()}, "${employee.first_name}", "${employee.last_name}", "${employee.role}", "placeholder", "5200000", "${employee.managers}");`)
+}
+
+
+
+function showDepartments(db){
     db.query(`SELECT * FROM departments`,function (err, results) {
         console.log(results)
     })
 }
 
 
-function showRoles(){
+function showRoles(db){
     db.query(`SELECT * FROM roles`,function (err, results) {
         console.log(results)
     })
 }
 
 
-function showEmployees(){
+function showEmployees(db){
     db.query(`SELECT * FROM employees`,function (err, results) {
         console.log(results)
     })
 }
 
-
-function mainMenu(db){
-inquirer
-  .prompt([
-    {
-      type: 'checkbox',
-      name: 'main',
-      message: 'What would you like to do?',
-      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
-    },
-]).then((result) => {
-    switch (result.main){
-        case "View all departments": showDepartments()
-            break
-        case "View all roles": showRoles()
-            break
-        case "View all employees": showEmployees()
-            break
-        case "Add a department": addDepartment(db)
-            break
-        case "Add a role": addRole(db)
-            break
-        case "Add an employee": addEmployee(db)
-            break
-        case "Update an employee role": showEmployees()
-            break
-}
-}).then(() => (mainMenu(db)))
-}
-
 module.exports ={
-  mainMenu,
   showRoles,
   showEmployees,
   showDepartments,
   addEmployee,
   addRole,
   addDepartment,
-  generateId
+  generateId,
+  add_department,
+  add_employee,
+  add_role
 }
--- server.js
+++ server.js
@@ -12,4 +12,28 @@ const db = mysql.createConnection(
   console.log(`Connected to the company_db database.`)
 );
 
-helpers.mainMenu()
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'main',
+      message: 'What would you like to do?',
+      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+    }
+]).then((result) => {
+    switch (result.main){
+        case "View all departments": helpers.showDepartments(db)
+            break
+        case "View all roles": helpers.showRoles(db)
+            break
+        case "View all employees": helpers.showEmployees(db)
+            break
+        case "Add a department": inquirer.prompt(helpers.add_department).then((result) => helpers.addDepartment(db, result)).then(() => console.log("Department added to database"))
+            break
+        case "Add a role": inquirer.prompt(helpers.add_role).then((result) => helpers.addRole(db, result)).then(() => console.log("Role added to database"))
+            break
+        case "Add an employee": inquirer.prompt(helpers.add_employee).then((result) => helpers.addEmployee(db, result)).then(() => console.log("Employee added to database"))
+            break
+        case "Update an employee role": inquirer.prompt(helpers.add_department)
+            break
+}})
